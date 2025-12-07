0b45d9b1ac9db758f0c75bd528d910651cf252b4
Md. Emon <115633465+MdEmonHowlader@users.noreply.github.com>
2025-12-07 21:43:34 +0600

    Refactor code structure for improved readability and maintainability

--- bangla_parser.tab.h
+++ bangla_parser.tab.h
@@ -1,22 +1,23 @@
-/* A Bison parser, made by GNU Bison 3.8.2.  */
 
-/* Bison interface for Yacc-like parsers in C
+/* A Bison parser, made by GNU Bison 2.4.1.  */
 
-   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
-   Inc.
-
+/* Skeleton interface for Bison's Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+   
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
-
+   
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-
+   
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -27,71 +28,54 @@
    special exception, which will cause the skeleton and the resulting
    Bison output files to be licensed under the GNU General Public
    License without this special exception.
-
+   
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
-   especially those whose name start with YY_ or yy_.  They are
-   private implementation details that can be changed or removed.  */
 
-#ifndef YY_YY_BANGLA_PARSER_TAB_H_INCLUDED
-# define YY_YY_BANGLA_PARSER_TAB_H_INCLUDED
-/* Debug traces.  */
-#ifndef YYDEBUG
-# define YYDEBUG 0
-#endif
-#if YYDEBUG
-extern int yydebug;
-#endif
-
-/* Token kinds.  */
+/* Tokens.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-  enum yytokentype
-  {
-    YYEMPTY = -2,
-    YYEOF = 0,                     /* "end of file"  */
-    YYerror = 256,                 /* error  */
-    YYUNDEF = 257,                 /* "invalid token"  */
-    TOKEN_SHONGKHA = 258,          /* TOKEN_SHONGKHA  */
-    TOKEN_DOSHOMIK = 259,          /* TOKEN_DOSHOMIK  */
-    TOKEN_LEKHA = 260,             /* TOKEN_LEKHA  */
-    TOKEN_PORO = 261,              /* TOKEN_PORO  */
-    TOKEN_DEKHAW = 262,            /* TOKEN_DEKHAW  */
-    OP_ASSIGN = 263,               /* OP_ASSIGN  */
-    SEMICOLON = 264,               /* SEMICOLON  */
-    TOKEN_IDENTIFIER = 265,        /* TOKEN_IDENTIFIER  */
-    TOKEN_SHONGKHA_VALUE = 266,    /* TOKEN_SHONGKHA_VALUE  */
-    TOKEN_DOSHOMIK_VALUE = 267,    /* TOKEN_DOSHOMIK_VALUE  */
-    TOKEN_LEKHA_VALUE = 268        /* TOKEN_LEKHA_VALUE  */
-  };
-  typedef enum yytokentype yytoken_kind_t;
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     TOKEN_SHONGKHA = 258,
+     TOKEN_DOSHOMIK = 259,
+     TOKEN_LEKHA = 260,
+     TOKEN_PORO = 261,
+     TOKEN_DEKHAW = 262,
+     OP_ASSIGN = 263,
+     SEMICOLON = 264,
+     TOKEN_IDENTIFIER = 265,
+     TOKEN_SHONGKHA_VALUE = 266,
+     TOKEN_DOSHOMIK_VALUE = 267,
+     TOKEN_LEKHA_VALUE = 268
+   };
 #endif
 
-/* Value type.  */
+
+
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-union YYSTYPE
+typedef union YYSTYPE
 {
+
+/* Line 1676 of yacc.c  */
 #line 58 "bangla_parser.y"
 
     char *str;
     int intVal;
     float floatVal;
 
-#line 83 "bangla_parser.tab.h"
 
-};
-typedef union YYSTYPE YYSTYPE;
+
+/* Line 1676 of yacc.c  */
+#line 73 "bangla_parser.tab.h"
+} YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
 #endif
-
 
 extern YYSTYPE yylval;
 
 
-int yyparse (void);
-
-
-#endif /* !YY_YY_BANGLA_PARSER_TAB_H_INCLUDED  */
